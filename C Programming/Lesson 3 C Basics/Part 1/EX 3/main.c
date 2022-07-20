02925fea6772b8b1dd057a77e3acded67f90fa34
Moaaz <moaaz5799@gmail.com>
2022-07-20 15:05:04 +0200

    Ex 3 and 4

---
+++ C Programming/Lesson 3 C Basics/Part 1/EX 3/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 3, 2022
+ *      Author: Moaaz Mansour
+ */
+#include "stdio.h"
+
+void main(){
+	int n1 = 0 , n2 = 0 ;
+	printf("Enter two numbers: ") ;
+	scanf("%d%d",&n1,&n2) ;
+	printf("sum : %d",n1+n2);
+	printf("\n###########################");
+}
---
+++ C Programming/Lesson 3 C Basics/Part 1/EX 4/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 3, 2022
+ *      Author: Moaaz Mansour
+ */
+#include "stdio.h"
+
+void main(){
+	float n1 = 0 , n2 = 0 ;
+	printf("Enter two numbers: ") ;
+	scanf("%f%f",&n1,&n2) ;
+	/*Long float because if the maximum number of 2 floats summed together or any similar case that will exceed the limit */
+	printf("sum : %lf",n1+n2);
+	printf("\n###########################");
+}

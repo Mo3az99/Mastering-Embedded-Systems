7652203abaa9d09aafccbbf42eec19a0fabba3f9
Moaaz <moaaz5799@gmail.com>
2022-07-20 14:50:18 +0200

    Ex 2

---
+++ C Programming/Lesson 3 C Basics/Part 1/EX 2/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 3, 2022
+ *      Author: Moaaz Mansour
+ */
+#include "stdio.h"
+
+void main(){
+	int user_input= 0  ;
+	printf("Enter an integer: ");
+	scanf("%d",&user_input);
+	printf("You entered: %d",user_input) ;
+	printf("\n###########################");
+}

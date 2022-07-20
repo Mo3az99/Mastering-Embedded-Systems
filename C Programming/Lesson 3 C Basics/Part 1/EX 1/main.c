8466d889c144e487b37778af42d114d98352c56a
Moaaz <moaaz5799@gmail.com>
2022-07-20 14:43:14 +0200

    Ex 1

---
+++ C Programming/Lesson 3 C Basics/Part 1/EX 1/main.c
@@ -0,0 +1,12 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 3, 2022
+ *      Author: Moaaz Mansour
+ */
+#include "stdio.h"
+
+void main(){
+	printf("C programming \n") ;
+	printf("###########################");
+}

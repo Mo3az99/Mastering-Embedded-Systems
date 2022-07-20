4a1fb4109a84158fa4a1ca2ad740bdc55fe4c85a
Moaaz <moaaz5799@gmail.com>
2022-07-20 15:07:39 +0200

    Ex 4 edit

--- C Programming/Lesson 3 C Basics/Part 1/EX 4/main.c
+++ C Programming/Lesson 3 C Basics/Part 1/EX 4/main.c
@@ -11,6 +11,6 @@ void main(){
 	printf("Enter two numbers: ") ;
 	scanf("%f%f",&n1,&n2) ;
 	/*Long float because if the maximum number of 2 floats summed together or any similar case that will exceed the limit */
-	printf("sum : %lf",n1+n2);
+	printf("Product : %lf",n1*n2);
 	printf("\n###########################");
 }

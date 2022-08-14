87a75f67fc96b381f9ccd0a4d63f536c53a955e7
Moaaz <moaaz5799@gmail.com>
2022-08-14 21:26:41 +0200

    count max number of ones between zeros (using &)

---
+++ C Programming/Midterm 1/Q10 MaxNumberOfOnes between two zeros/main.c
@@ -0,0 +1,35 @@
+/*
+ ============================================================================
+ Name        : main.c
+ Author      : Moaaz Mansour
+ Version     :
+ Copyright   :
+ Description : Assignment in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int maxOnes(int number){
+	char count = 0 ;
+	while (number != 0){
+		number = (number & (number<<1));
+		count++;
+	}
+	return count;
+}
+int main(void) {
+	int number ;
+	printf("Enter Number : ");
+	fflush(stdout);
+	scanf("%d",&number);
+	fflush(stdin);
+	printf("Max ones are %d ",maxOnes(number));
+	fflush(stdout);
+
+	return EXIT_SUCCESS;
+}
+

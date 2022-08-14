3dccb1caefd4d8dd9ce1fd56435e2832708d8d67
Moaaz <moaaz5799@gmail.com>
2022-08-14 19:48:19 +0200

    Reverse Digits in Number

--- C Programming/Midterm 1/ReverseDigitsInNumber/main.c
+++ C Programming/Midterm 1/ReverseDigitsInNumber/main.c
@@ -15,16 +15,16 @@
 
 
 int main(void) {
-	int n, reverse=0, rem;
+	int number, reverse=0, rem;
 	printf("Enter a number: ");
 	fflush(stdout);
-	scanf("%d", &n);
+	scanf("%d", &number);
 	fflush(stdin);
-	while(n!=0)
+	while(number!=0)
 	{
-		rem=n%10;
+		rem=number%10;
 		reverse=reverse*10+rem;
-		n/=10;
+		number/=10;
 	}
 	printf("Reversed Number: %d",reverse);
 	fflush(stdout);

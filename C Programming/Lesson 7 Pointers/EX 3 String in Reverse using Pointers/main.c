d7af6429e515c84b5dc26710eb57c910ba4b3b9c
Moaaz <moaaz5799@gmail.com>
2022-09-04 00:01:25 +0200

    EX 3 String in reverse using Pointers

--- C Programming/Lesson 7 Pointers/EX 3 String in Reverse using Pointers/main.c
+++ C Programming/Lesson 7 Pointers/EX 3 String in Reverse using Pointers/main.c
@@ -14,14 +14,16 @@
 #include <math.h>
 
 int main(void) {
-	char x[27];
-	char *ptr = x;
-	int i;
-	for(i=0; i<26; i++,++ptr)
-		*ptr = 'A' + i;
-	ptr =x;
-	for(i=0; i<26; i++,ptr++)
-		printf("%c   ",*ptr);
+	char x[11];
+	char *p = NULL;
+	printf("Enter String : ");
+	fflush(stdout);
+	scanf("%s",x);
+	fflush(stdin);
+	p = &x[strlen(x)];
+	for(int i = 1 ;i <= strlen(x) ; i++){
+		printf("%c",*(p-i));
+	}
 	return EXIT_SUCCESS;
 }
 

a62c629eda7f897b41415978cd19918120bcf05b
Moaaz <moaaz5799@gmail.com>
2022-09-11 01:28:22 +0200

    Toggle LED

---
+++ Embedded C/Lesson 1/can.h
@@ -0,0 +1,14 @@
+/*
+ * Can.h
+ *
+ *  Created on: Sep 11, 2022
+ *      Author: Moaaz Mansour
+ */
+#include <stdio.h>
+
+#ifndef CAN_H_
+#define CAN_H_
+ extern void can_init();
+
+
+#endif /* CAN_H_ */
---
+++ Embedded C/Lesson 1/main.c
@@ -0,0 +1,7 @@
+#include "can.h"
+#include <stdio.h>
+
+int void main(){
+	can_init();
+	return 0;
+}

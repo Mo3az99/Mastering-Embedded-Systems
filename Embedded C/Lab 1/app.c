7ac481d55794210852b2eda096c5434dfcfdf70a
Moaaz <moaaz5799@gmail.com>
2022-09-13 02:17:53 +0200

    application made from scratch by hand (linker + startup)

---
+++ Embedded C/Lab 1/app.c
@@ -0,0 +1,11 @@
+#include "uart.h"
+
+unsigned char buffer [100] = "learn-in-depth: Moaaz";
+unsigned char const buffer2 [100] = "learn-in-depth: Moaaz";
+
+
+void main(void){
+	
+	uart_send_String(buffer);
+	
+}
